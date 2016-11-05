656df317222d1e97b13240ffe46bf78858b064dc
LemonAtSu <at_7788546@hotmail.com>
2016-11-05 16:00:57 +0800

    first commit

---
+++ src/proc_data.py
@@ -0,0 +1,37 @@
+ # -*- coding: utf-8 -*-
+import numpy as np
+import re
+from bs4 import BeautifulSoup
+
+def parse_html(text):
+    text = BeautifulSoup(text, 'html.parser')
+    
+    author = text.find('span', attrs={'class': 'author_name'})
+    title = text.find('h1', attrs={'class': 'title'}).get_text()
+    content = []
+    
+    ps = text.findAll('p')
+    for p in ps:
+        content.append(p.get_text())
+
+    try:
+        content = author.join(content)
+    except :
+        content = "".join(content)
+        
+    r = '(?::|;|=|X)(?:-)?(?:\)|\(|D|P)'
+    emoticons = re.findall(r, content)
+    content = re.sub(r, '', content)
+    
+    content = re.sub('[\W]+', ' ', content.lower()) + ' ' + ' '.join(emoticons).replace('-','')
+    return title, content
+
+def parse_raw(raw_data):
+    parsed = np.empty((raw_data.shape[0], 2), dtype=object)
+
+    for r in range(raw_data.shape[0]):
+        title, content = (parse_html(raw_data[r]))
+        parsed[r, 0] = title
+        parsed[r, 1] = content
+
+    return parsed
