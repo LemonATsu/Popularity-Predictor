7715e0298612440e26d5a0758bbed46000c8914a
LemonAtSu <at_7788546@hotmail.com>
2016-11-10 13:07:03 +0800

    seg

--- src/proc_data.py
+++ src/proc_data.py
@@ -2,27 +2,45 @@
 import numpy as np
 import re
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
+
+def parse_channel(text):
+    text = BeautifulSoup(text, 'html.parser')
+    channel = text.find('article')['data-channel']
+    return channel
+
+def parse_date(date):
+    d = parse(date)
+    weekday = d.weekday()
+    times = (d.second + d.minute*60 + d.hour*3600) / 86400.0
+    return weekday, times
 
 def parse_html(text):
     text = BeautifulSoup(text, 'html.parser')
-
+    n_fig = 0
     if text.find('div', attrs={'class' : 'see-also'}):    
         text.find('div', attrs={'class' : 'see-also'}).decompose()  
     if text.find('div', attrs={'class' : 'bonus-content'}):
         text.find('div', attrs={'class' : 'bonus-content'}).decompose()
     if text.findAll('div', attrs={'class' : 'meta'}):
         [meta.decompose() for meta in text.findAll('div', attrs={'class' : 'meta'})]
+    if text.findAll('figure'):
+        n_fig = len(text.findAll('figure'))
 
-    author = text.find('span', attrs={'class': 'author_name'})
     title = text.find('h1', attrs={'class': 'title'}).get_text()
     content = []
     
     ps = text.findAll('p')
     for p in ps:
         content.append(p.get_text())
-
+    author =  "none"
     try:
-        content = author.join(content)
+        author = text.find('span', attrs={'class': 'author_name'}).get_text()
+        temp = author.split(" ")[1:]
+        author = ""
+        for part in temp:
+            author += part
+        content = "".join(content) 
     except :
         content = "".join(content)
         
@@ -33,7 +51,7 @@ def parse_html(text):
     content = re.sub('[\W]+', ' ', content.lower()) + ' ' + ' '.join(emoticons).replace('-','')
     title = re.sub('[\W]+', ' ', title.lower()) + ' ' + ' '.join(emoticons).replace('-','')
 
-    return title + " " + content
+    return title, content, author, n_fig
 
 def parse_raw(raw_data):
     parsed = np.empty((raw_data.shape[0], 2), dtype=object)
